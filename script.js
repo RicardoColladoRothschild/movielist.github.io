cbeda8f5d239ac1b7941cc29f566f9ae4a0112ac
ricardocollado <ricardocolladorothschild@gmail.com>
2023-04-14 17:35:55 -0400

    create style for the little div that we create from script, and show author information

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 7f9f2c096c04f228306760754dd17a92*/
 
 let autor = document.querySelector('#aboutAuthor');
-
+let form1 = document.querySelector('#form');
 //API REST DATA:
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=7f9f2c096c04f228306760754dd17a92&query="';
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7f9f2c096c04f228306760754dd17a92&page=1';
@@ -14,6 +14,8 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 //after click on autor, button call an anonimous function, and show author (me) name and info
 autor.addEventListener('click', ()=>{
     let divAutorInformation = document.createElement('div');
-    console.log('rickyboy');
+    divAutorInformation.classList.add('authorInfo');
+        form1.append(divAutorInformation);
+
 
 });
